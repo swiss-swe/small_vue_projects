cfb73c88d62f76f03ea4a9003d488ab30766a82c
UMAR <139427461+MrUmarDev@users.noreply.github.com>
2023-12-10 05:25:13 -0800

    Delete BookWebsite directory

--- BookWebsite/src/router/notfound.js
+++
@@ -1,5 +0,0 @@
-export const notFoundRoutes = {
-  path: "/:pathMatch(.*)*",
-  name: "not-found",
-  component: () => import("../views/errors/404.vue")
-};
--- BookWebsite/src/views/pages/Users.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="ml-[25%] w-[75%] bg-slate-600">Users</div>
-</template>
-
-<script setup></script>
-
-<style scoped></style>
